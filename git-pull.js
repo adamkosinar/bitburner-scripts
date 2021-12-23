0a072a0099bd9afce7140cc767a028c1695c6e88
adamkosinar <adam.kosinar@venatusmedia.com>
2021-12-23 19:47:45 +0000

    added git pull

--- git-pull.js
+++ git-pull.js
@@ -1,6 +1,21 @@
 /** @param {NS} ns **/
 export async function main(ns) {
 
+    const endpoint = "https://raw.githubusercontent.com/adamkosinar/bitburner-scripts/master";
+
+    const files = [
+        "/git-pull.js"
+    ];
+
+
+    for (let i = 0; i < files.length; i++) {
+
+        let file = files[i];
+
+        await ns.wget(endpoint + file, file);
+
+        ns.print("INFO--- PULLING NEW FILE FROM GIT: " + file);
+    }
 
 
 }
